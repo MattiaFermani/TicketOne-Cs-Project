fd4c0bfb937af06ed8b0b2ae6914414fcd021662
MATTIA FERMANI <s24909@iisve.it>
2025-03-07 22:32:20 +0100

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -279,6 +279,10 @@
             this.Pannello_Login = new System.Windows.Forms.Panel();
             this.tabControl2 = new System.Windows.Forms.TabControl();
             this.Pgn_Register = new System.Windows.Forms.TabPage();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.Register_Btn = new System.Windows.Forms.Button();
             this.Comuni_Lst = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
@@ -290,10 +294,6 @@
             this.label3 = new System.Windows.Forms.Label();
             this.Pgn_Login = new System.Windows.Forms.TabPage();
             this.Login_Btn = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.TickeTlon)).BeginInit();
             this.panel1.SuspendLayout();
@@ -611,10 +611,10 @@
             this.Pgn_Informazioni.Controls.Add(this.groupBox1);
             this.Pgn_Informazioni.Controls.Add(this.Img_Info);
             this.Pgn_Informazioni.Controls.Add(this.TitoloArtista);
-            this.Pgn_Informazioni.Location = new System.Drawing.Point(4, 25);
+            this.Pgn_Informazioni.Location = new System.Drawing.Point(4, 22);
             this.Pgn_Informazioni.Name = "Pgn_Informazioni";
             this.Pgn_Informazioni.Padding = new System.Windows.Forms.Padding(3);
-            this.Pgn_Informazioni.Size = new System.Drawing.Size(1143, 405);
+            this.Pgn_Informazioni.Size = new System.Drawing.Size(1143, 408);
             this.Pgn_Informazioni.TabIndex = 0;
             this.Pgn_Informazioni.Text = "Informazioni";
             // 
@@ -637,7 +637,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(284, 33);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 25);
+            this.label2.Size = new System.Drawing.Size(44, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Data";
             // 
@@ -647,7 +647,7 @@
             this.Data_Lst.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Data_Lst.FormattingEnabled = true;
             this.Data_Lst.HorizontalScrollbar = true;
-            this.Data_Lst.ItemHeight = 25;
+            this.Data_Lst.ItemHeight = 20;
             this.Data_Lst.Location = new System.Drawing.Point(288, 55);
             this.Data_Lst.Name = "Data_Lst";
             this.Data_Lst.Size = new System.Drawing.Size(184, 100);
@@ -659,7 +659,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(10, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 25);
+            this.label1.Size = new System.Drawing.Size(54, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Luogo";
             // 
@@ -669,7 +669,7 @@
             this.Luogo_Lst.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Luogo_Lst.FormattingEnabled = true;
             this.Luogo_Lst.HorizontalScrollbar = true;
-            this.Luogo_Lst.ItemHeight = 25;
+            this.Luogo_Lst.ItemHeight = 20;
             this.Luogo_Lst.Location = new System.Drawing.Point(13, 55);
             this.Luogo_Lst.Name = "Luogo_Lst";
             this.Luogo_Lst.Size = new System.Drawing.Size(281, 100);
@@ -712,7 +712,7 @@
             this.Artista_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Artista_Lbl.Location = new System.Drawing.Point(9, 71);
             this.Artista_Lbl.Name = "Artista_Lbl";
-            this.Artista_Lbl.Size = new System.Drawing.Size(67, 25);
+            this.Artista_Lbl.Size = new System.Drawing.Size(55, 20);
             this.Artista_Lbl.TabIndex = 1;
             this.Artista_Lbl.Text = "Artista";
             // 
@@ -722,7 +722,7 @@
             this.TitoloSpettacolo_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TitoloSpettacolo_Lbl.Location = new System.Drawing.Point(7, 26);
             this.TitoloSpettacolo_Lbl.Name = "TitoloSpettacolo_Lbl";
-            this.TitoloSpettacolo_Lbl.Size = new System.Drawing.Size(115, 42);
+            this.TitoloSpettacolo_Lbl.Size = new System.Drawing.Size(95, 36);
             this.TitoloSpettacolo_Lbl.TabIndex = 0;
             this.TitoloSpettacolo_Lbl.Text = "Titolo";
             // 
@@ -737,10 +737,10 @@
             this.Pgn_SelezionePosti.Controls.Add(this.Settore_E);
             this.Pgn_SelezionePosti.Controls.Add(this.Settore_A);
             this.Pgn_SelezionePosti.Controls.Add(this.Palco);
-            this.Pgn_SelezionePosti.Location = new System.Drawing.Point(4, 25);
+            this.Pgn_SelezionePosti.Location = new System.Drawing.Point(4, 22);
             this.Pgn_SelezionePosti.Name = "Pgn_SelezionePosti";
             this.Pgn_SelezionePosti.Padding = new System.Windows.Forms.Padding(3);
-            this.Pgn_SelezionePosti.Size = new System.Drawing.Size(1143, 405);
+            this.Pgn_SelezionePosti.Size = new System.Drawing.Size(1143, 408);
             this.Pgn_SelezionePosti.TabIndex = 1;
             this.Pgn_SelezionePosti.Tag = "Normal";
             this.Pgn_SelezionePosti.Text = "Selezione Posti";
@@ -3871,13 +3871,45 @@
             this.Pgn_Register.Controls.Add(this.label4);
             this.Pgn_Register.Controls.Add(this.textBox1);
             this.Pgn_Register.Controls.Add(this.label3);
-            this.Pgn_Register.Location = new System.Drawing.Point(4, 25);
+            this.Pgn_Register.Location = new System.Drawing.Point(4, 22);
             this.Pgn_Register.Name = "Pgn_Register";
             this.Pgn_Register.Padding = new System.Windows.Forms.Padding(3);
-            this.Pgn_Register.Size = new System.Drawing.Size(1143, 405);
+            this.Pgn_Register.Size = new System.Drawing.Size(1143, 408);
             this.Pgn_Register.TabIndex = 0;
             this.Pgn_Register.Text = "Registrazione";
             this.Pgn_Register.Click += new System.EventHandler(this.Pgn_Register_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(392, 122);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(253, 20);
+            this.textBox4.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(389, 106);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "email";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(392, 56);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(253, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(389, 40);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "email";
             // 
             // Register_Btn
             // 
@@ -3891,10 +3923,12 @@
             // 
             // Comuni_Lst
             // 
+            this.Comuni_Lst.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.Comuni_Lst.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.Comuni_Lst.FormattingEnabled = true;
             this.Comuni_Lst.Location = new System.Drawing.Point(49, 262);
             this.Comuni_Lst.Name = "Comuni_Lst";
-            this.Comuni_Lst.Size = new System.Drawing.Size(252, 24);
+            this.Comuni_Lst.Size = new System.Drawing.Size(252, 21);
             this.Comuni_Lst.TabIndex = 10;
             // 
             // label6
@@ -3902,7 +3936,7 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(46, 246);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(117, 16);
+            this.label6.Size = new System.Drawing.Size(94, 13);
             this.label6.TabIndex = 9;
             this.label6.Text = "Comune di nascita";
             // 
@@ -3911,7 +3945,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(46, 179);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(96, 16);
+            this.label5.Size = new System.Drawing.Size(78, 13);
             this.label5.TabIndex = 7;
             this.label5.Text = "Data di nascita";
             // 
@@ -3919,14 +3953,14 @@
             // 
             this.dateTimePicker1.Location = new System.Drawing.Point(48, 195);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(253, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(253, 20);
             this.dateTimePicker1.TabIndex = 6;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(47, 122);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(254, 22);
+            this.textBox2.Size = new System.Drawing.Size(254, 20);
             this.textBox2.TabIndex = 3;
             // 
             // label4
@@ -3934,7 +3968,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(44, 106);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(66, 16);
+            this.label4.Size = new System.Drawing.Size(52, 13);
             this.label4.TabIndex = 2;
             this.label4.Text = "Cognome";
             // 
@@ -3942,7 +3976,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(48, 56);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(253, 22);
+            this.textBox1.Size = new System.Drawing.Size(253, 20);
             this.textBox1.TabIndex = 1;
             // 
             // label3
@@ -3950,17 +3984,17 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(45, 40);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 0;
             this.label3.Text = "Nome";
             // 
             // Pgn_Login
             // 
             this.Pgn_Login.Controls.Add(this.Login_Btn);
-            this.Pgn_Login.Location = new System.Drawing.Point(4, 25);
+            this.Pgn_Login.Location = new System.Drawing.Point(4, 22);
             this.Pgn_Login.Name = "Pgn_Login";
             this.Pgn_Login.Padding = new System.Windows.Forms.Padding(3);
-            this.Pgn_Login.Size = new System.Drawing.Size(1143, 405);
+            this.Pgn_Login.Size = new System.Drawing.Size(1143, 408);
             this.Pgn_Login.TabIndex = 1;
             this.Pgn_Login.Text = "Login";
             this.Pgn_Login.UseVisualStyleBackColor = true;
@@ -3974,38 +4008,6 @@
             this.Login_Btn.Text = "Accedi";
             this.Login_Btn.UseVisualStyleBackColor = true;
             this.Login_Btn.Click += new System.EventHandler(this.Login_Register);
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(392, 56);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(253, 22);
-            this.textBox3.TabIndex = 13;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(389, 40);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(40, 16);
-            this.label7.TabIndex = 12;
-            this.label7.Text = "email";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(392, 122);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(253, 22);
-            this.textBox4.TabIndex = 15;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(389, 106);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(40, 16);
-            this.label8.TabIndex = 14;
-            this.label8.Text = "email";
             // 
             // Form1
             // 
--- Form1.cs
+++ Form1.cs
@@ -18,78 +18,114 @@ namespace Biglietti_concerto
         static Random rand = new Random();
         static List<string> Comuni = new List<string>();
         static List<string> CodiciBelfiore = new List<string>();
-
-        static List<string> DataRandom(Random rand)
+        readonly static List<string> Luoghi = new List<string>
         {
-            List<string> dates = new List<string>();
-            for (int i = 0; i < 4; i++)
+            "Firenze - Teatro Verdi",
+            "Roma - Stadio Olimpico",
+            "Roma - Auditorium Parco della Musica",
+            "Torino - Teatro Regio",
+            "Verona - Arena di Verona",
+            "Milano - Teatro La Scala",
+            "Milano - Blue Note",
+            "Torino - Pala Alpitour",
+            "Roma - Palazzetto dello Sport",
+            "Napoli - Teatro Centrale",
+            "Firenze - Stadio Artemio Franchi",
+            "Roma - Ippodromo delle Capannelle",
+            "Milano - Mediolanum Forum",
+            "Bologna - Unipol Arena",
+            "Milano - Alcatraz",
+            "Roma - Atlantico",
+            "Bologna - Estragon Club",
+            "Napoli - Palapartenope",
+            "Roma - Circo Massimo",
+            "Milano - Ippodromo Snai",
+            "Roma - Auditorium della Musica",
+            "Firenze - Palazzo dei Congressi",
+            "Palermo - Teatro Massimo",
+            "Roma - Villa Ada",
+            "Milano - Fabrique",
+            "Genova - Politeama Genovese",
+            "Milano - Teatro Manzoni",
+            "Milano - Teatro Elfo Puccini",
+            "Milano - Auditorium San Fedele",
+            "Torino - Teatro Gobetti",
+            "Bari - Stadio San Nicola",
+            "Milano - Teatro degli Arcimboldi",
+            "Roma - Palazzo dello Sport",
+            "Milano - Stadio San Siro"
+        };
+
+        static List<string> Date_Luoghi_Random(int cosa, Random rand)
+        {
+            switch (cosa)
             {
-                int day = rand.Next(1, 29);
-                int month = rand.Next(4, 13);
-                int year = 2025 + rand.Next(0, 2);
-                dates.Add($"{year:D4}/{month:D2}/{day:D2}");
+                case 0:
+                    List<string> luoghi = new List<string>();
+                    for (int i = 0; i < 4; i++)
+                    {
+                        int index = rand.Next(0, Luoghi.Count);
+                        luoghi.Add(Luoghi[index]);
+                    }
+                    return luoghi;
+                case 1:
+                    List<string> dates = new List<string>();
+                    for (int i = 0; i < 4; i++)
+                    {
+                        int day = rand.Next(1, 29);
+                        int month = rand.Next(4, 13);
+                        int year = 2025 + rand.Next(0, 2);
+                        dates.Add($"{year:D4}/{month:D2}/{day:D2}");
+                    }
+
+                    dates.Sort();
+                    dates.Reverse();
+
+                    for (int i = 0; i < dates.Count; i++)
+                    {
+                        DateTime dt = DateTime.ParseExact(dates[i], "yyyy/MM/dd", null);
+                        dates[i] = dt.ToString("dd/MM/yyyy");
+                    }
+
+                    dates.Reverse();
+                    return dates;
             }
-
-            dates.Sort();
-            dates.Reverse();
-
-            for (int i = 0; i < dates.Count; i++)
-            {
-                DateTime dt = DateTime.ParseExact(dates[i], "yyyy/MM/dd", null);
-                dates[i] = dt.ToString("dd/MM/yyyy");
-            }
-
-            dates.Reverse();
-            return dates;
+            return null;
         }
+
         static Dictionary<string, (List<string> luoghi, List<string> date)> Eventi = new Dictionary<string, (List<string>, List<string>)>
         {
-            { "Intelligenza Naturale",
-                (new List<string> { "Firenze - Teatro Verdi", "Roma - Stadio Olimpico", "Roma - Auditorium Parco della Musica", "Torino - Teatro Regio" },
-                DataRandom(rand)) },
-            { "Marcus Miller", (new List<string> { "Verona - Arena di Verona", "Milano - Teatro La Scala", "Milano - Blue Note", "Torino - Pala Alpitour" },
-                DataRandom(rand)) },
-            { "LRDL Summer Tour 2025", (new List<string> { "Roma - Palazzetto dello Sport", "Napoli - Teatro Centrale", "Firenze - Stadio Artemio Franchi", "Roma - Ippodromo delle Capannelle" },
-                DataRandom(rand)) },
-            { "PalaJova", (new List<string> { "Torino - Pala Alpitour", "Milano - Mediolanum Forum", "Bologna - Unipol Arena", "Roma - Stadio Olimpico" },
-                DataRandom(rand)) },
-            { "Sophie and The Giants", (new List<string> { "Milano - Alcatraz", "Roma - Atlantico", "Firenze - Teatro Verdi", "Bologna - Estragon Club" },
-                DataRandom(rand)) },
-            { "Damme na mano Roma e Milano", (new List<string> { "Milano - Ippodromo Snai", "Roma - Circo Massimo", "Napoli - Palapartenope", "Bologna - Unipol Arena" },
-                DataRandom(rand)) },
-            { "Games in Concert", (new List<string> { "Roma - Auditorium della Musica", "Torino - Teatro Regio", "Firenze - Palazzo dei Congressi", "Palermo - Teatro Massimo" },
-                DataRandom(rand)) },
-            { "FASK tour estivo 2025", (new List<string> { "Roma - Villa Ada", "Bologna - Estragon Club", "Milano - Fabrique", "Milano - Alcatraz" },
-                DataRandom(rand)) },
-            { "Prova A Prendermi", (new List<string> { "Roma - Teatro Brancaccio", "Milano - Teatro Nazionale", "Genova - Politeama Genovese", "Bologna - Teatro Comunale" },
-                DataRandom(rand)) },
-            { "Vita Bassa", (new List<string> { "Milano - Teatro Manzoni", "Milano - Teatro Elfo Puccini", "Milano - Auditorium San Fedele", "Torino - Teatro Gobetti" },
-                DataRandom(rand)) },
-            { "Estate 2025", (new List<string> { "Milano - Ippodromo Snai San Siro", "Roma - Circo Massimo", "Firenze - Stadio Artemio Franchi", "Bari - Stadio San Nicola" },
-                DataRandom(rand)) },
-            { "Jimmy Sax and Symphonic Dance Orchestra", (new List<string> { "Milano - Teatro degli Arcimboldi", "Roma - Auditorium Parco della Musica", "Firenze - Palazzo della Musica", "Verona - Arena di Verona" },
-                DataRandom(rand)) },
-            { "2025 World Tour - Milano", (new List<string> { "Milano - Mediolanum Forum", "Bologna - Unipol Arena", "Torino - Pala Alpitour", "Roma - Palazzo dello Sport" },
-                DataRandom(rand)) },
-            { "AC/DC - Powerup Tour", (new List<string> { "Milano - Stadio San Siro", "Roma - Stadio Olimpico", "Verona - Arena di Verona", "Roma - Ippodromo delle Capannelle" },
-                DataRandom(rand)) }
+            { "Intelligenza Naturale", (Date_Luoghi_Random(0, rand), Date_Luoghi_Random(1, rand)) },
+            { "Marcus Miller", (Date_Luoghi_Random(0, rand), Date_Luoghi_Random(1, rand)) },
+            { "LRDL Summer Tour 2025", (Date_Luoghi_Random(0, rand), Date_Luoghi_Random(1, rand)) },
+            { "PalaJova", (Date_Luoghi_Random(0, rand), Date_Luoghi_Random(1, rand)) },
+            { "Sophie and The Giants", (Date_Luoghi_Random(0, rand), Date_Luoghi_Random(1, rand)) },
+            { "Damme na mano Roma e Milano", (Date_Luoghi_Random(0, rand), Date_Luoghi_Random(1, rand)) },
+            { "Games in Concert", (Date_Luoghi_Random(0, rand), Date_Luoghi_Random(1, rand)) },
+            { "FASK tour estivo 2025", (Date_Luoghi_Random(0, rand), Date_Luoghi_Random(1, rand)) },
+            { "Prova A Prendermi", (Date_Luoghi_Random(0, rand), Date_Luoghi_Random(1, rand)) },
+            { "Vita Bassa", (Date_Luoghi_Random(0, rand), Date_Luoghi_Random(1, rand)) },
+            { "Estate 2025", (Date_Luoghi_Random(0, rand), Date_Luoghi_Random(1, rand)) },
+            { "Jimmy Sax and Symphonic Dance Orchestra", (Date_Luoghi_Random(0, rand), Date_Luoghi_Random(1, rand)) },
+            { "2025 World Tour - Milano", (Date_Luoghi_Random(0, rand), Date_Luoghi_Random(1, rand)) },
+            { "AC/DC - Powerup Tour", (Date_Luoghi_Random(0, rand), Date_Luoghi_Random(1, rand)) }
         };
         List<(string Titolo, string Artista, string Descrizione, Dictionary<string, (List<string> luoghi, List<string> date)> Dizionario)> Spettacoli = new List<(string, string, string, Dictionary<string, (List<string>, List<string>)>)>
-    {
-        ("Intelligenza Naturale", "Andrea Pezzi", "Uno spettacolo sull'intelligenza umana", Eventi),
-        ("Marcus Miller", "Marcus Miller", "Il maestro del basso funk torna in Italia", Eventi),
-        ("LRDL Summer Tour 2025", "La Rappresentante di Lista", "Musica e performance incredibili", Eventi),
-        ("PalaJova", "Jovanotti", "Annunciate nuove date. Scopri i dettagli e acquista il tuo biglietto!", Eventi),
-        ("Sophie and The Giants", "Sophie\nThe Giants", "Annunciato un nuovo appuntamento. Scopri i dettagli e acquista il tuo biglietto!", Eventi),
-        ("Damme na mano Roma e Milano", "Tony Effe", "Annunciati nuovi appuntamenti live. Scopri i dettagli e acquista il tuo biglietto!", Eventi),
-        ("Games in Concert", "Autore Sconosciuto", "Le colonne sonore dei più celebri videogames. Scopri i dettagli e acquista il tuo biglietto!", Eventi),
-        ("FASK tour estivo 2025", "Fast Animals and Slow Kids", "Annunciate nuove date estive. Scopri i dettagli!", Eventi),
-        ("Prova A Prendermi", "Guido Castrogiovanni\nTommaso Cassissa", "Arriva il musical basato sul film di Steven Spielberg con Leonardo DiCaprio e Tom Hanks.\nScopri i dettagli e acquista il tuo biglietto!", Eventi),
-        ("Vita Bassa", "Giorgia Fumo", "Scopri i dettagli e acquista il tuo biglietto!", Eventi),
-        ("Estate 2025", "Morrissey", "Scopri i dettagli e acquista il tuo biglietto!", Eventi),
-        ("Jimmy Sax and Symphonic Dance Orchestra", "Jimmy Sax", "Scopri i dettagli e acquista il tuo biglietto!", Eventi),
-        ("2025 World Tour - Milano", "Black Pink", "Le Blackpink, star mondiali del K-POP, arrivano in Italia per la prima volta.\nScopri i dettagli!", Eventi),
-        ("AC/DC - Powerup Tour", "AC/DC", "Annunciata una data estiva del POWER UP Tour. Scopri i dettagli!", Eventi),
+        {
+            ("Intelligenza Naturale", "Andrea Pezzi", "Uno spettacolo sull'intelligenza umana", Eventi),
+            ("Marcus Miller", "Marcus Miller", "Il maestro del basso funk torna in Italia", Eventi),
+            ("LRDL Summer Tour 2025", "La Rappresentante di Lista", "Musica e performance incredibili", Eventi),
+            ("PalaJova", "Jovanotti", "Annunciate nuove date. Scopri i dettagli e acquista il tuo biglietto!", Eventi),
+            ("Sophie and The Giants", "Sophie\nThe Giants", "Annunciato un nuovo appuntamento. Scopri i dettagli e acquista il tuo biglietto!", Eventi),
+            ("Damme na mano Roma e Milano", "Tony Effe", "Annunciati nuovi appuntamenti live. Scopri i dettagli e acquista il tuo biglietto!", Eventi),
+            ("Games in Concert", "Autore Sconosciuto", "Le colonne sonore dei più celebri videogames. Scopri i dettagli e acquista il tuo biglietto!", Eventi),
+            ("FASK tour estivo 2025", "Fast Animals and Slow Kids", "Annunciate nuove date estive. Scopri i dettagli!", Eventi),
+            ("Prova A Prendermi", "Guido Castrogiovanni\nTommaso Cassissa", "Arriva il musical basato sul film di Steven Spielberg con Leonardo DiCaprio e Tom Hanks.\nScopri i dettagli e acquista il tuo biglietto!", Eventi),
+            ("Vita Bassa", "Giorgia Fumo", "Scopri i dettagli e acquista il tuo biglietto!", Eventi),
+            ("Estate 2025", "Morrissey", "Scopri i dettagli e acquista il tuo biglietto!", Eventi),
+            ("Jimmy Sax and Symphonic Dance Orchestra", "Jimmy Sax", "Scopri i dettagli e acquista il tuo biglietto!", Eventi),
+            ("2025 World Tour - Milano", "Black Pink", "Le Blackpink, star mondiali del K-POP, arrivano in Italia per la prima volta.\nScopri i dettagli!", Eventi),
+            ("AC/DC - Powerup Tour", "AC/DC", "Annunciata una data estiva del POWER UP Tour. Scopri i dettagli!", Eventi),
         };
 
         private void ComuniITA(string filePath)
