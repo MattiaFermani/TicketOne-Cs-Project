4ea0e44067867d6cbee027e9cc6bb0e13086f4f2
NOEMI BURINI <s24905@iisve.it>
2025-03-08 09:39:13 +0100

    ...

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -279,6 +279,8 @@
             this.Pannello_Login = new System.Windows.Forms.Panel();
             this.tabControl2 = new System.Windows.Forms.TabControl();
             this.Pgn_Register = new System.Windows.Forms.TabPage();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.Register_Btn = new System.Windows.Forms.Button();
             this.Comuni_Lst = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
@@ -290,9 +292,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.Pgn_Login = new System.Windows.Forms.TabPage();
             this.Login_Btn = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.label8 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.TickeTlon)).BeginInit();
@@ -3858,8 +3859,9 @@
             // 
             // Pgn_Register
             // 
-            this.Pgn_Register.Controls.Add(this.textBox4);
             this.Pgn_Register.Controls.Add(this.label8);
+            this.Pgn_Register.Controls.Add(this.radioButton2);
+            this.Pgn_Register.Controls.Add(this.radioButton1);
             this.Pgn_Register.Controls.Add(this.textBox3);
             this.Pgn_Register.Controls.Add(this.label7);
             this.Pgn_Register.Controls.Add(this.Register_Btn);
@@ -3878,6 +3880,22 @@
             this.Pgn_Register.TabIndex = 0;
             this.Pgn_Register.Text = "Registrazione";
             this.Pgn_Register.Click += new System.EventHandler(this.Pgn_Register_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(392, 56);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(253, 22);
+            this.textBox3.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(389, 40);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "email";
             // 
             // Register_Btn
             // 
@@ -3975,37 +3993,36 @@
             this.Login_Btn.UseVisualStyleBackColor = true;
             this.Login_Btn.Click += new System.EventHandler(this.Login_Register);
             // 
-            // textBox3
+            // radioButton1
             // 
-            this.textBox3.Location = new System.Drawing.Point(392, 56);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(253, 22);
-            this.textBox3.TabIndex = 13;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(391, 125);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(39, 20);
+            this.radioButton1.TabIndex = 14;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "M";
+            this.radioButton1.UseVisualStyleBackColor = true;
             // 
-            // label7
+            // radioButton2
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(389, 40);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(40, 16);
-            this.label7.TabIndex = 12;
-            this.label7.Text = "email";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(392, 122);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(253, 22);
-            this.textBox4.TabIndex = 15;
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(391, 151);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(36, 20);
+            this.radioButton2.TabIndex = 15;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "F";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(389, 106);
+            this.label8.Location = new System.Drawing.Point(388, 106);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(40, 16);
-            this.label8.TabIndex = 14;
-            this.label8.Text = "email";
+            this.label8.Size = new System.Drawing.Size(44, 16);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "sesso";
             // 
             // Form1
             // 
@@ -4334,7 +4351,8 @@
         private System.Windows.Forms.Button Login_Btn;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Label label8;
     }
 }
