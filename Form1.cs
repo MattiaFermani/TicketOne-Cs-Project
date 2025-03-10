e405725a268ce63496bcf111a1a4edfbb61a7e6d
Mattia <ilviperone.ita@gmail.com>
2025-03-10 10:49:14 +0100

    .

--- Form1.cs
+++ Form1.cs
@@ -481,8 +481,15 @@ namespace Biglietti_concerto
             {
                 {'0', 1}, {'1', 0}, {'2', 5}, {'3', 7}, {'4', 9}, {'5', 13}, {'6', 15}, {'7', 17}, {'8', 19}, {'9', 21},
                 {'A', 1}, {'B', 0}, {'C', 5}, {'D', 7}, {'E', 9}, {'F', 13}, {'G', 15}, {'H', 17}, {'I', 19}, {'J', 21},
-                {'K', 1}, {'L', 0}, {'M', 5}, {'N', 7}, {'O', 9}, {'P', 13}, {'Q', 15}, {'R', 17}, {'S', 19}, {'T', 21},
-                {'U', 1}, {'V', 0}, {'W', 5}, {'X', 7}, {'Y', 9}, {'Z', 13}
+                {'K', 2}, {'L', 4}, {'M', 18}, {'N', 20}, {'O', 11}, {'P', 3}, {'Q', 6}, {'R', 8}, {'S', 12}, {'T', 14},
+                {'U', 16}, {'V', 10}, {'W', 22}, {'X', 25}, {'Y', 24}, {'Z', 23}
+            };
+
+            Dictionary<int, char> Carattere = new Dictionary<int, char>
+            {
+                {0, 'A'}, {1, 'B'}, {2, 'C'}, {3, 'D'}, {4, 'E'}, {5, 'F'}, {6, 'G'}, {7, 'H'}, {8, 'I'}, {9, 'J'},
+                {10, 'K'}, {11, 'L'}, {12, 'M'}, {13, 'N'}, {14, 'O'}, {15, 'P'}, {16, 'Q'}, {17, 'R'}, {18, 'S'}, {19, 'T'},
+                {20, 'U'}, {21, 'V'}, {22, 'W'}, {23, 'X'}, {24, 'Y'}, {25, 'Z'}
             };
             cf = cf.ToUpper();
             int somma = 0;
@@ -495,8 +502,7 @@ namespace Biglietti_concerto
                     somma += Dispari[c];
             }
 
-            cf += (char)('A' + (somma % 26));
-            cf = cf.ToLower();
+            cf += Carattere[somma % 26];
 
             return cf;
         }
@@ -709,16 +715,15 @@ namespace Biglietti_concerto
             }
             string comune = Comuni_Lst.SelectedItem.ToString();
 
-            txt_codicefiscale.Text = CalcCodiceFiscale(nome, cognome, dataNascita, sesso, comune);
 
-            //if(string.Equals(txt_codicefiscale.Text, CalcCodiceFiscale(nome, cognome, dataNascita, sesso, comune), StringComparison.OrdinalIgnoreCase))
-            //{
-            //    txt_codicefiscale.BackColor = Color.Green;
-            //}
-            //else
-            //{
-            //    txt_codicefiscale.BackColor = Color.Red;
-            //}
+            if (string.Equals(txt_codicefiscale.Text, CalcCodiceFiscale(nome, cognome, dataNascita, sesso, comune), StringComparison.OrdinalIgnoreCase))
+            {
+                txt_codicefiscale.BackColor = Color.Green;
+            }
+            else
+            {
+                txt_codicefiscale.BackColor = Color.Red;
+            }
         }
 
 
